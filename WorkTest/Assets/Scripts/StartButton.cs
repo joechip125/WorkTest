2df66d4e3045cdf8a1b0ac2ea9fc8d19e93cd8f0
Mattias Lönnqvist <joechip125@gmail.com>
2023-01-30 18:54:19 +0100

    created new material

--- WorkTest/Assets/Scripts/StartButton.cs
+++ WorkTest/Assets/Scripts/StartButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour, IPointerClickHandler
 {
-    
     public void OnPointerClick(PointerEventData eventData)
     {
         SceneManager.LoadScene("Level1", LoadSceneMode.Single);
