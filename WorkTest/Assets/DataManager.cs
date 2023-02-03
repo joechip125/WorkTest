b5b3ec8957f702ebefdcf2dd11ce6088c93be934
Mattias Lönnqvist <joechip125@gmail.com>
2023-02-03 14:58:23 +0100

    update

--- WorkTest/Assets/DataManager.cs
+++ WorkTest/Assets/DataManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,24 +11,39 @@ public class DataManager : MonoBehaviour
 {
     private float _lastTime;
     private string _path;
-    private MultiData _allData;
+    private List<PlayerData> _allData = new();
+    [SerializeField] TextHolder textHolder;
+
 
     private void Awake()
     {
         _lastTime = PlayerPrefs.GetFloat("FinalTime");
-        //_path = Application.persistentDataPath + "/gameData.json";
-        _path = Application.persistentDataPath + "/testData.json";
+        _path = Application.persistentDataPath + "/gameData.json";
+        //_path = Application.persistentDataPath + "/testData.json";
         LoadData();
         
     }
 
-    private void LoadData()
+    private void CheckList()
+    {
+        if (_allData.Count > 9)
+        {
+            if (_allData.Any(x => x.playerScore > _lastTime))
+            {
+                
+            }
+        }
+    }
+
+    public void LoadData()
     {
         if (File.Exists(_path))
         {
             var fileContents = File.ReadAllBytes(_path);
             string result = Encoding.Default.GetString(fileContents);
             var data = JsonUtility.FromJson<MultiData>(result);
+            _allData = data.playerData;
+            textHolder.UpdateRankings(_allData);
         }
         else
         {
@@ -37,9 +53,13 @@ public class DataManager : MonoBehaviour
 
     public void SaveData()
     {
-       _allData.playerData.Add(new PlayerData("berk", 15));
-       var data = JsonUtility.ToJson(_allData);
-       var bytes  = Encoding.Default.GetBytes(data);
+       _allData.Add(new PlayerData("berk", 15));
+       var allDat = new MultiData()
+       {
+           playerData = _allData
+       };
+       var data = JsonUtility.ToJson(allDat);
+       var bytes = Encoding.Default.GetBytes(data);
        File.WriteAllBytes(_path, bytes);
     }
     
--- WorkTest/Assets/TextHolder.cs
+++ WorkTest/Assets/TextHolder.cs
@@ -12,6 +12,17 @@ public class TextHolder : MonoBehaviour
         _texts = GetComponentsInChildren<TextMeshProUGUI>().ToList();
     }
 
+    public void UpdateRankings(List<PlayerData> data)
+    {
+        for (int i = 0; i < _texts.Count; i++)
+        {
+            if (data.Count > i)
+            {
+                _texts[i].text = $"{data[i].playerName}: {data[i].playerScore}";
+            }
+        }
+    }
+
     void Update()
     {
         
